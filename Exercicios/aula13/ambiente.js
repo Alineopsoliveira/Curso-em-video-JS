d50b49b6a26b035b815d5976c1b5dd64c0b1eafe
Alineopsoliveira <alinep730@gmail.com>
2023-06-13 15:38:21 -0300

    exercicio 13

---
+++ Exercicios/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 6) /* estrutura de repetição com teste lógico no final*/ 
+
+var d = 1
+while(d <= 6){
+    console.log(`Teste ${d}`)
+    d++
+} /*Estrutura de repetição com teste lógico no inicio*/ 
