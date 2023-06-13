f18a9d614cf999fcd831d6c422505c66bd912955
Alineopsoliveira <alinep730@gmail.com>
2023-06-13 16:47:37 -0300

    organização de arquivos e execução de aula 14

---
+++ Exercicios/aula14/ambiente.js
@@ -0,0 +1,15 @@
+ var a = 1
+ while(a <= 5){
+    console.log(a)
+    a++
+ }
+console.log('//----------------------------//')
+ var b = 1
+ do{
+    console.log(b)
+    b++
+ }while(b <= 5)
+ console.log('//----------------------------//')
+ for(var c = 1; c <= 5; c++){
+    console.log(c)
+ }
