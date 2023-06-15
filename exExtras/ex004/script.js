5e0de54f55e2dad48a1fa87e1d6abc56de2d16ee
Alineopsoliveira <alinep730@gmail.com>
2023-06-14 22:27:46 -0300

    adicionei mais alguns exercicios

---
+++ exExtras/ex004/script.js
@@ -0,0 +1,13 @@
+  var resultado = window.document.querySelector('section#resultado')
+function primeiro(){
+    resultado.innerHTML = '<p>Você clicou no primeiro botão</p>'
+}
+function segundo(){
+    resultado.innerHTML += '<p>Você clicou no segundo botão</p>'
+}
+function terceiro(){
+    resultado.innerHTML += '<p>Você clicou no terceiro botão</p>'
+}
+function quarto(){
+    resultado.innerHTML += '<p>Você clicou no quarto botão</p>'
+}
